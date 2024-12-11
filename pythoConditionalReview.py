91274da08090d5ebcd1b6ac0f38feaf863499acd
Samyde <smcneil900@boyslatin.net>
2024-12-11 13:47:51 +0000

    im finnish doing the conditictinal

---
+++ pythoConditionalReview.py
@@ -0,0 +1,15 @@
+# function are instructions to be used on data/
+
+# Conditionals use i/ else keyword to make desitions 
+# and outcomes based on deta.
+
+def elevatorLogic(floor):
+# 1. take to user input/ user deta. --< use perameter
+# 2. determine which floor they want to go to based on the input\
+    if floor == 'b
+    print('you are going to the basement.')'
+elif floor== '1':
+print('if you are going to the lobby')
+elif floor == 1:
+print('if you are going the first floor.')
+elif 
