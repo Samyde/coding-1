ac447a268ccf99ee1dcd816e2c37faa6eec4ddf3
Samyde <smcneil900@boyslatin.net>
2024-12-19 13:46:50 +0000

    finnish the holliday extra credit

---
+++ unit1/asynActivitydec13md.m
@@ -0,0 +1,8 @@
+def positiveOrNegative(number):
+   if number > -1:
+    print('this is a positive number')
+    else: number < 0
+    print('number not recongnize')
+    else+
+    -
+    positiveOrNegative(1)
