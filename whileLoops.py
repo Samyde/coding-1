1f1b068afa17c6674bc7833643376e1daa9bbfbf
Samyde <smcneil900@boyslatin.net>
2025-01-22 13:42:56 +0000

    Notes 1/17/25

---
+++ whileLoops.py
@@ -0,0 +1,21 @@
+# Loops - Is a construct that repeats a set of
+# instructions under specific conditions.
+
+
+# while loops - so long as a conditions is true,
+# keep repeating the loop.
+
+x= 3
+y= 'samyde'
+while x == 2:
+while y == 'samyde'
+while 10 < 1:
+     print('this text will repeat forever')
+
+expiration = imput ('what is todays date')
+
+while expiration == ('1/23/25')
+      print (' this account has expired.')
+      imput ('what is todays date)
+        
+
